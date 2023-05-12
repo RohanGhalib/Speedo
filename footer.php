e61a6168ab7a914c2041bc47f61efc56aeec84c0
RohanGhalib <muhammadrohanghalib@gmail.com>
2023-05-12 09:47:59 +0500

    first commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+<div class="clearfix"></div>
+        <!-- Footer -->
+        <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                        Copyright &copy; 2023 Society EShuttle Service
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        Project Made by <a href="https://tezistantech.ga/digicv/cv">Muhammad Rohan Ghalib</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- /.site-footer -->
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include("header.php");?>
+<div class="right-panel">
+<div class="content">
+            <div class="animated fadeIn">
+            <div class="row">
+            <div class="col-lg-11">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="progress-box progress-1">
+                                    <h4 class="por-title">Shuttle GT</h4>
+                                    <div class="por-txt"><b>Fareed Park (50%)</b></div>
+                                    <div class="progress mb-2" style="height: 5px;">
+                                        <div class="progress-bar bg-flat-color-1" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-box progress-2">
+                                    <h4 class="por-title">Shuttle GW:</h4>
+                                    <div class="por-txt"><b>Tanoor (30%)</b></div>
+                                    <div class="progress mb-2" style="height: 5px;">
+                                        <div class="progress-bar bg-flat-color-2" role="progressbar" style="width: 30%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-box progress-1">
+                                    <h4 class="por-title">Shuttle CC</h4>
+                                    <div class="por-txt"><b>White House (90%)</b></div>
+                                    <div class="progress mb-2" style="height: 5px;">
+                                        <div class="progress-bar bg-flat-color-1" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                                <div class="progress-box progress-1">
+                                    <h4 class="por-title">Shuttle GC</h4>
+                                    <div class="por-txt"><b>Doggy House (20%)</b></div>
+                                    <div class="progress mb-2" style="height: 5px;">
+                                        <div class="progress-bar bg-flat-color-1" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                </div>
+                            </div> <!-- /.card-body -->
+                        </div>
+                    </div>
+          
+</div>
+</div>
+</div>
+</div>
+<?php include("footer.php");?>
